46b2565cad60f3b1f522dd5b35c917bb24816621
dennis <wakahiad@gmail.com>
2022-04-17 17:13:23 +0300

    create category function

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/densoft/shopmeAdmin/category/CategoryController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/densoft/shopmeAdmin/category/CategoryController.java
@@ -1,11 +1,18 @@
 package com.densoft.shopmeAdmin.category;
 
+import com.densoft.shopmeAdmin.util.FileUpload;
 import com.densoft.shopmecommon.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -32,5 +39,16 @@ public class CategoryController {
         model.addAttribute("listCategories", categoryLIst);
         model.addAttribute("pageTitle", "Create new category");
         return "categories/category_form";
+    }
+
+    @PostMapping("/categories/save")
+    public String saveCategory(Category category, @RequestParam("fileImage") MultipartFile multipartFile, RedirectAttributes redirectAttributes) throws IOException {
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        category.setImage(fileName);
+        Category savedCategory = categoryService.save(category);
+        String uploadDir = "../category-images/" + savedCategory.getId();
+        FileUpload.saveFile(uploadDir, fileName, multipartFile);
+        redirectAttributes.addFlashAttribute("message", "Category saved successfully");
+        return "redirect:/categories";
     }
 }
--- ShopmeWebParent/ShopmeBackend/src/main/java/com/densoft/shopmeAdmin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/densoft/shopmeAdmin/category/CategoryService.java
@@ -18,17 +18,21 @@ public class CategoryService {
         return categoryRepository.findAll();
     }
 
+    public Category save(Category category) {
+        return categoryRepository.save(category);
+    }
+
     public List<Category> listCategoriesUsedInForm() {
         List<Category> categoriesUsedInForm = new ArrayList<>();
         List<Category> categoriesInDb = categoryRepository.findAll();
 
         for (Category category : categoriesInDb) {
             if (category.getParent() == null) {
-                categoriesUsedInForm.add(new Category(category.getName()));
+                categoriesUsedInForm.add(Category.copyIdAndName(category));
                 Set<Category> children = category.getChildren();
                 for (Category subCategory : children) {
                     String name = "--" + subCategory.getName();
-                    categoriesUsedInForm.add(new Category(name));
+                    categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
                     listChildren(categoriesUsedInForm, subCategory, 1);
                 }
             }
@@ -47,7 +51,7 @@ public class CategoryService {
                 name += "--";
             }
             name += subCategory.getName();
-            categoriesUsedInForm.add(new Category(name));
+            categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
             listChildren(categoriesUsedInForm, subCategory, newSubLevel);
         }
     }
