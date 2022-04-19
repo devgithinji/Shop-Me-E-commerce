7040a63fa50da4c24c51217f2d9fa29ed2b5df44
dennis <wakahiad@gmail.com>
2022-04-19 12:17:24 +0300

    update category

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/densoft/shopmeAdmin/category/CategoryController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/densoft/shopmeAdmin/category/CategoryController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -43,12 +44,36 @@ public class CategoryController {
 
     @PostMapping("/categories/save")
     public String saveCategory(Category category, @RequestParam("fileImage") MultipartFile multipartFile, RedirectAttributes redirectAttributes) throws IOException {
-        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
-        category.setImage(fileName);
-        Category savedCategory = categoryService.save(category);
-        String uploadDir = "../category-images/" + savedCategory.getId();
-        FileUpload.saveFile(uploadDir, fileName, multipartFile);
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            category.setImage(fileName);
+            Category savedCategory = categoryService.save(category);
+            String uploadDir = "../category-images/" + savedCategory.getId();
+            FileUpload.cleanDir(uploadDir);
+            FileUpload.saveFile(uploadDir, fileName, multipartFile);
+        } else {
+            categoryService.save(category);
+        }
+
         redirectAttributes.addFlashAttribute("message", "Category saved successfully");
         return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/edit/{id}")
+    public String editCategory(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+
+            Category category = categoryService.get(id);
+            List<Category> categories = categoryService.listCategoriesUsedInForm();
+            model.addAttribute("category", category);
+            model.addAttribute("listCategories", categories);
+            model.addAttribute("pageTitle", "Edit Category (ID: " + id + ")");
+            return "categories/category_form";
+
+        } catch (CategoryNotFoundException categoryNotFoundException) {
+            redirectAttributes.addFlashAttribute("message", categoryNotFoundException.getMessage());
+            return "redirect:/categories";
+        }
+
     }
 }
--- ShopmeWebParent/ShopmeBackend/src/main/java/com/densoft/shopmeAdmin/category/CategoryService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/densoft/shopmeAdmin/category/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -87,5 +88,14 @@ public class CategoryService {
             categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
             listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, newSubLevel);
         }
+    }
+
+    public Category get(Integer id) throws CategoryNotFoundException {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+
+        if (categoryOptional.isPresent()) {
+            return categoryOptional.get();
+        }
+        throw new CategoryNotFoundException("Could not find any category with ID: " + id);
     }
 }
